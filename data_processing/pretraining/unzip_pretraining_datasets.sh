5f6058b69c59670fadff425e0e89cc82462bfb55
orlem <orllem3456@gmail.com>
2023-04-25 21:10:17 -0300

    bash script to unzip pretraining data

---
+++ data_processing/pretraining/unzip_pretraining_datasets.sh
@@ -0,0 +1,94 @@
+
+# TAU-2018
+
+## ANSYN
+
+mkdir -p ${DATA_WORK_PATH}/TUT_2018/ANSYN
+cd ${DATA_PATH}/TUT_2018/ANSYN/
+unzip -DD ov1_split1.zip -d ${DATA_WORK_PATH}/TUT_2018/ANSYN
+unzip -DD ov1_split2.zip -d ${DATA_WORK_PATH}/TUT_2018/ANSYN
+unzip -DD ov1_split3.zip -d ${DATA_WORK_PATH}/TUT_2018/ANSYN
+unzip -DD ov2_split1.zip -d ${DATA_WORK_PATH}/TUT_2018/ANSYN
+unzip -DD ov2_split2.zip -d ${DATA_WORK_PATH}/TUT_2018/ANSYN
+unzip -DD ov2_split3.zip -d ${DATA_WORK_PATH}/TUT_2018/ANSYN
+unzip -DD ov3_split1.zip -d ${DATA_WORK_PATH}/TUT_2018/ANSYN
+unzip -DD ov3_split2.zip -d ${DATA_WORK_PATH}/TUT_2018/ANSYN
+unzip -DD ov3_split3.zip -d ${DATA_WORK_PATH}/TUT_2018/ANSYN
+cd ${DATA_PATH}
+
+## REAL
+
+mkdir -p ${DATA_WORK_PATH}/TUT_2018/REAL
+cd ${DATA_PATH}/TUT_2018/REAL/
+unzip -DD ov1_split1.zip -d ${DATA_WORK_PATH}/TUT_2018/REAL
+unzip -DD ov1_split8.zip -d ${DATA_WORK_PATH}/TUT_2018/REAL
+unzip -DD ov1_split9.zip -d ${DATA_WORK_PATH}/TUT_2018/REAL
+unzip -DD ov2_split1.zip -d ${DATA_WORK_PATH}/TUT_2018/REAL
+unzip -DD ov2_split8.zip -d ${DATA_WORK_PATH}/TUT_2018/REAL
+unzip -DD ov2_split9.zip -d ${DATA_WORK_PATH}/TUT_2018/REAL
+unzip -DD ov3_split1.zip -d ${DATA_WORK_PATH}/TUT_2018/REAL
+unzip -DD ov3_split8.zip -d ${DATA_WORK_PATH}/TUT_2018/REAL
+unzip -DD ov3_split9.zip -d ${DATA_WORK_PATH}/TUT_2018/REAL
+cd ${DATA_PATH}
+
+
+# TAU-2019
+
+mkdir -p ${DATA_WORK_PATH}/TAU_2019
+cd ${DATA_PATH}/TAU_2019/
+unzip -DD metadata_dev.zip -d ${DATA_WORK_PATH}/TAU_2019
+unzip -DD foa_dev_full.zip -d ${DATA_WORK_PATH}/TAU_2019
+unzip -DD mic_dev_full.zip -d ${DATA_WORK_PATH}/TAU_2019
+
+unzip -DD metadata_eval.zip -d ${DATA_WORK_PATH}/TAU_2019
+unzip -DD foa_eval.zip -d ${DATA_WORK_PATH}/TAU_2019
+unzip -DD mic_eval.zip -d ${DATA_WORK_PATH}/TAU_2019
+cd ${DATA_PATH} 
+
+
+# TAU-2020
+
+mkdir -p ${DATA_WORK_PATH}/TAU_2020
+cd ${DATA_PATH}/TAU_2020/
+unzip -DD metadata_dev.zip -d ${DATA_WORK_PATH}/TAU_2020
+unzip -DD foa_dev_full.zip -d ${DATA_WORK_PATH}/TAU_2020
+unzip -DD mic_dev_full.zip -d ${DATA_WORK_PATH}/TAU_2020
+
+unzip -DD metadata_eval.zip -d ${DATA_WORK_PATH}/TAU_2020
+unzip -DD foa_eval.zip -d ${DATA_WORK_PATH}/TAU_2020
+unzip -DD mic_eval.zip -d ${DATA_WORK_PATH}/TAU_2020
+cd ${DATA_PATH} 
+
+
+# TAU-2021
+
+mkdir -p ${DATA_WORK_PATH}/TAU_2021
+cd ${DATA_PATH}/TAU_2021/
+unzip -DD metadata_dev.zip -d ${DATA_WORK_PATH}/TAU_2021
+unzip -DD foa_dev_full.zip -d ${DATA_WORK_PATH}/TAU_2021
+unzip -DD mic_dev_full.zip -d ${DATA_WORK_PATH}/TAU_2021
+
+unzip -DD metadata_eval.zip -d ${DATA_WORK_PATH}/TAU_2021
+unzip -DD foa_eval.zip -d ${DATA_WORK_PATH}/TAU_2021
+unzip -DD mic_eval.zip -d ${DATA_WORK_PATH}/TAU_2021
+cd ${DATA_PATH} 
+
+# L3DAS21
+
+mkdir -p ${DATA_WORK_PATH}/L3DAS21
+cd ${DATA_PATH}/L3DAS21/
+unzip -DD L3DAS_Task1_dev.zip -d ${DATA_WORK_PATH}/L3DAS21
+unzip -DD L3DAS_Task1_test.zip -d ${DATA_WORK_PATH}/L3DAS21
+unzip -DD L3DAS_Task1_train100.zip -d ${DATA_WORK_PATH}/L3DAS21
+unzip -DD L3DAS_Task1_train360.zip -d ${DATA_WORK_PATH}/L3DAS21
+unzip -DD L3DAS_Task2_dev.zip -d ${DATA_WORK_PATH}/L3DAS21
+unzip -DD L3DAS_Task2_test.zip -d ${DATA_WORK_PATH}/L3DAS21
+unzip -DD L3DAS_Task2_train.zip -d ${DATA_WORK_PATH}/L3DAS21
+cd ${DATA_PATH} 
+
+# L3DAS22
+
+mkdir -p ${DATA_WORK_PATH}/L3DAS22
+cd ${DATA_PATH}/L3DAS22/
+unzip -DD l3das22.zip -d ${DATA_WORK_PATH}/L3DAS22
+cd ${DATA_PATH} 
